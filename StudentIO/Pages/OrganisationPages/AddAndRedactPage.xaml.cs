aeb4542c3859873c9dfc9b92dd5686f4245de7e0
Sergat 777 <sergey.kond777@gmail.com>
2023-02-16 00:24:24 +0300

    added AddRedact confirmation

--- StudentIO/Pages/OrganisationPages/AddAndRedactPage.xaml.cs
+++ StudentIO/Pages/OrganisationPages/AddAndRedactPage.xaml.cs
@@ -42,7 +42,43 @@ namespace StudentIO.Pages.OrganisationPages
 
         private void btConfirm_Click(object sender, RoutedEventArgs e)
         {
+            if (!string.IsNullOrWhiteSpace(tbSecondNameEmployee.Text) &&
+                !string.IsNullOrWhiteSpace(tbFirstNameEmployee.Text) &&
+                !string.IsNullOrWhiteSpace(tbLoginEmployee.Text) &&
+                !string.IsNullOrWhiteSpace(tbPasswordEmployee.Text))
+            {
+                if (!tbSecondNameEmployee.Text.Any(Char.IsDigit) &&
+                    !tbFirstNameEmployee.Text.Any(Char.IsDigit) &&
+                    !tbMiddleNameEmployee.Text.Any(Char.IsDigit))
+                {
+                    if (MessageBox.Show("Вы уверены что хотите сохранить информацию об учетной записи сотрудника?", "Внимание",
+                        MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                    {
+                        AddRedactEmployee.SecondNameEmployee = tbSecondNameEmployee.Text.Trim();
+                        AddRedactEmployee.FirstNameEmployee = tbFirstNameEmployee.Text.Trim();
 
+                        if (!string.IsNullOrWhiteSpace(tbMiddleNameEmployee.Text))
+                            AddRedactEmployee.MiddleNameEmloyee = tbMiddleNameEmployee.Text.Trim();
+
+                        AddRedactEmployee.Login = tbLoginEmployee.Text;
+                        AddRedactEmployee.Password = tbPasswordEmployee.Text;
+
+                        if (AddRedactEmployee.IdEmployee == 0)
+                            DataBase.StudentIOEntities.GetContext().Employee.Add(AddRedactEmployee);
+
+                        DataBase.StudentIOEntities.GetContext().SaveChanges();
+
+                        MessageBox.Show("Информация успешно сохранена!", "Информация",
+                            MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+                }
+                else
+                    MessageBox.Show("Поля ФИО заполненны некорректно!", "Внимание", 
+                        MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else
+                MessageBox.Show("Все поля обязательны для заполнения!", "Внимание",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
         }
     }
 }
